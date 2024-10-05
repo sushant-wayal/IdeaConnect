2928a52c76b91589031772dad38335fb09fbb0dc
sushant-wayal <sushantssr12345@gmail.com>
2024-10-05 13:36:43 +0530

    Added code for deployment

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 import { app } from './app.js';
+import path from 'path';
+import express from 'express';
 
 dotenv.config({
   path: './.env',
@@ -13,6 +15,18 @@ connectDB()
   });
   app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    
+    // Deployement code
+
+    const __dirname1 = path.resolve();
+    if (process.env.PROJECT_MODE === 'production') {
+      app.use(express.static(path.join(__dirname1, '/Frontend/dist')));
+      app.get('*', (_req, res) =>
+        res.sendFile(path.resolve(__dirname1, 'Frontend', 'dist', 'index.html'))
+      );
+    }
+
+    // Deployement code
   });
 })
 .catch((error) => {
